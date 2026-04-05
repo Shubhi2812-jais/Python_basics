664f5a1a7682d8765b104b48e19c851a2ff33a11
Shubhangi Jaiswal <shubhangii2812@gmail.com>
2026-04-05 14:14:03 +0530

    d:\Python Folder\Python_basics\Chapter 2\Average.py

---
+++ Chapter 2/Average.py
@@ -0,0 +1,6 @@
+#  Input 2 numbers and print average
+
+input1=int(input("Enter first number:"))
+input2=int(input("Enter second number:"))
+average=(input1+input2)/2
+print("Average of 2 values is:",average)
