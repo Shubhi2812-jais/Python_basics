540fbdedd203b407393a2cc4fa63ee07916b7815
Shubhangi Jaiswal <shubhangii2812@gmail.com>
2026-04-10 12:13:33 +0530

    d:\Python Folder\Python_basics\Chapter 2\Twonumbers.py

---
+++ Chapter 2/Twonumbers.py
@@ -0,0 +1,14 @@
+x=2
+y=8
+sum= x+y
+difference= x-y
+product= x*y
+print("Sum of two numbers is:",sum)
+print("Difference of two numbers is:",difference)
+print("Product of two numbers is:",product)
+
+x=2
+y=8
+print("Sum of two numbers is:",x+y)
+print("Difference of two numbers is:",x-y)
+print("Porduct of two numbers is:",x*y)
