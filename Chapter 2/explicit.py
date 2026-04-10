e368a5b37cb726dd27e45953933f37776c54dd0f
Shubhangi Jaiswal <shubhangii2812@gmail.com>
2026-04-10 12:05:16 +0530

    d:\Python Folder\Python_basics\Chapter 2\explicit.py

---
+++ Chapter 2/explicit.py
@@ -0,0 +1,6 @@
+# Take num as input, convert to float and print both original & converted value with their data types
+
+num= input("Enter a value:")
+convertedValue= float(num)
+print("Original value is:",num,"Data Type:",type(num))
+print("Converted value is:",convertedValue,"Data Type:",type(convertedValue))
