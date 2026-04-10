313df85597da6de8a04368a646f12a16c5cbbcef
Shubhangi Jaiswal <shubhangii2812@gmail.com>
2026-04-10 12:03:13 +0530

    d:\Python Folder\Python_basics\Chapter 2\billSplit.py

---
+++ Chapter 2/billSplit.py
@@ -0,0 +1,7 @@
+total_bill=float(input("Enter total bill amount pay:"))
+friends=int(input("Enter number of friends:"))
+amount_per_person=total_bill/friends
+print("Each person will pay:",amount_per_person)
+print("Data type of total_bill",type(total_bill))
+print("Data type of friends:",type(friends))
+print("Data type of amount_per_person",type(amount_per_person))
