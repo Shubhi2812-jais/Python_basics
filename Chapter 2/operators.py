f213e540fcbd8a2b3f58070862ef27f401e7efc9
Shubhangi Jaiswal <shubhangii2812@gmail.com>
2026-04-10 12:09:47 +0530

    d:\Python Folder\Python_basics\Chapter 2\operators.py

---
+++ Chapter 2/operators.py
@@ -0,0 +1,31 @@
+#  arithmetic operators
+
+x=10
+y=5
+print(x+y)
+print(x-y) 
+print(x*y)
+print(x/y)
+print(x%y)
+print(x**y)
+
+# comparison operators
+print(x==y) #false
+print(x>=y) #true
+print(x<=y) #false
+print(x>y) #true
+print((x<y)) #false
+
+# logical operators
+
+print(x>y and x<y) #false
+print(x>y or x<y) #true
+print(not(x>y)) #false
+print(not(x<y)) #true
+
+# assignment operators
+a=2
+b=3
+
+print("a=+6")
+print("a-=1")
