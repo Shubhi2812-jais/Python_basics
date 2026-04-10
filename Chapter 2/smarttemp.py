d836d727f6e8cc1b83e23276dd72d20e9e31028d
Shubhangi Jaiswal <shubhangii2812@gmail.com>
2026-04-10 12:12:46 +0530

    d:\Python Folder\Python_basics\Chapter 2\smarttemp.py

---
+++ Chapter 2/smarttemp.py
@@ -0,0 +1,6 @@
+celsius=float(input("Enter tempertaure in celsius:"))
+fahrenheit=(celsius * 9/5) +32
+kelvin= celsius + 273.15
+
+print("Temperature in fahrenheit:",fahrenheit)
+print("Temperature in kelvin:",kelvin)
