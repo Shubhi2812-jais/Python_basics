d80bddcdda3b0383eb3a475ae4baa9d59406ef1c
Shubhangi Jaiswal <shubhangii2812@gmail.com>
2026-04-11 10:28:54 +0530

    d:\Python Folder\Python_basics\Chapter 3\assignmentchpter.py

---
+++ Chapter 3/assignmentchpter.py
@@ -0,0 +1,7 @@
+text=input("Enter any word or sentence:")
+first_char=text[0]
+last_char=text[-1]
+total_char=len(text)
+print("First Character:",first_char)
+print("Last Character:",last_char)
+print("Total number of characters:",total_char)
