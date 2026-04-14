9c496e09ff1d8c853f8d115eaeb48e2fd1a76e57
Shubhangi Jaiswal <shubhangii2812@gmail.com>
2026-04-14 15:53:05 +0530

    d:\Python Folder\Python_basics\Chapter 3\slicing.py

---
+++ Chapter 3/slicing.py
@@ -0,0 +1,13 @@
+str= "GulabJamun"
+firstHalf=str[0:5]
+print(firstHalf)
+trailHalf= str[:6]
+print(trailHalf)
+TrialHalf=str[5:9]
+print(TrialHalf)
+halfTrial=str[5:]
+print(halfTrial)
+
+#negative indexing
+str="JamunGulab"
+print(str[-5:-1])
