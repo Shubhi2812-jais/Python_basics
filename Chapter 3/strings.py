0784ddb6e57d14361d982472735cdce0f6e43c3c
Shubhangi Jaiswal <shubhangii2812@gmail.com>
2026-04-16 22:27:48 +0530

    d:\Python Folder\Python_basics\Chapter 3\strings.py

---
+++ Chapter 3/strings.py
@@ -0,0 +1,12 @@
+str1='Hello'
+str2="Shubhangi Jaiswal"
+str3='''Welcome to Python'''
+print(str1)
+print(str2)
+print(str3)
+
+# string concatenation
+print(str1+" "+str2)
+
+# length of string
+print(len(str1))
