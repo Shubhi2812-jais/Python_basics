ae011cb8ec490dfa92171190d681349809ca7524
Shubhangi Jaiswal <shubhangii2812@gmail.com>
2026-04-23 13:00:36 +0530

    d:\Python Folder\Python_basics\Chapter 4\practices.py

---
+++ Chapter 4/practices.py
@@ -0,0 +1,11 @@
+# program that takes a number as input and prints"
+
+number= int(input("Enter a number:"))
+if(number>0):
+    print("Positive")
+elif(number==0):
+    print("Zero")
+elif(number<0):
+    print("Negative")
+else:
+    print("Nothing")
