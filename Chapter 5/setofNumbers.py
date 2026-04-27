f4b36a8464210006ad977e931719413abe7848e0
Shubhangi Jaiswal <shubhangii2812@gmail.com>
2026-04-27 23:30:28 +0530

    d:\Python Folder\Python_basics\Chapter 5\setofNumbers.py

---
+++ Chapter 5/setofNumbers.py
@@ -0,0 +1,8 @@
+set1={1,2,3,4,5}
+set2={4,5,6,7,8}
+union_set=set1.union(set2)
+intersection_set=set1.intersection(set2)
+print("set1:",set1)
+print("set2:",set2)
+print("Union of set 1 and set 2:",union_set)
+print("Intersection of set 1 and set 2:",intersection_set)
