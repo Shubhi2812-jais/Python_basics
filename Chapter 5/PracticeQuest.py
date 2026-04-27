b131d089d99c26ac5c5abf326d77d2ef8734b45e
Shubhangi Jaiswal <shubhangii2812@gmail.com>
2026-04-27 23:29:47 +0530

    d:\Python Folder\Python_basics\Chapter 5\PracticeQuest.py

---
+++ Chapter 5/PracticeQuest.py
@@ -0,0 +1,4 @@
+programmingList=["Python","Java","C++","Python","Java","C"]
+# How to convert List into Set
+programmingSet= set(programmingList)
+print("Divya knows unique languages:",len(programmingSet))
