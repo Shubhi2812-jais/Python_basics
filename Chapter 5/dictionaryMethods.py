4ad9f901e6bcd8b50a95826cdc8a74a42d459d57
Shubhangi Jaiswal <shubhangii2812@gmail.com>
2026-04-27 23:26:06 +0530

    d:\Python Folder\Python_basics\Chapter 5\dictionaryMethods.py

---
+++ Chapter 5/dictionaryMethods.py
@@ -0,0 +1,12 @@
+student= {
+    "name": "Shubhangi Jaiswal",
+    "city": "Motihari",
+    "age":22,
+    "rollNumber": 33
+}
+print(type(student))
+print(student.keys())
+print(student.values())
+print(student.items())
+print(student.get("name"))
+print(student.update({"city":"lucknow"}))
