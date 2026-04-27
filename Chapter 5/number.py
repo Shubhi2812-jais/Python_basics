60d43fecc56788bdd0e0f4500b4eb036c51b5175
Shubhangi Jaiswal <shubhangii2812@gmail.com>
2026-04-27 23:29:08 +0530

    d:\Python Folder\Python_basics\Chapter 5\number.py

---
+++ Chapter 5/number.py
@@ -0,0 +1,13 @@
+number=set()
+number.add(9)
+number.add(9.0)
+print("Set after adding 9 and 9.0:",number)
+print("Length of set:",len(number))
+
+# making one number into string
+number_unique=set()
+number_unique.add(9)
+number_unique.add("9.0")
+
+print("\n Set after adding 9 and'9.0':",number_unique)
+print("Length of Set:",len(number_unique))
